5bad7e4a6c77d345348525c85c1b33103dc87ac2
Pauline BERGHEAUD <pauline.bergheaud@devinci.fr>
2014-11-20 12:45:13 +0100

    update space false

--- index.php
+++ index.php
@@ -3,19 +3,6 @@ require __DIR__.'/vendor/autoload.php';
 
 $PassGenerator = new \Web1\StringGenerator\PasswordGenerator();
 
-
 //$PassGenerator->charact();
 
 echo \Web1\StringGenerator\PasswordGenerator::charact(50, \Web1\StringGenerator\PasswordGenerator::PASSWORD_HARD);
-
-
-
-
-
-
-
-
-
-
-
-
